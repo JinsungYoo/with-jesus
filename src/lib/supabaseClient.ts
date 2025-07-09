b7167f3661456bf31bd5912414824d1d74862f33
Jinsung YOO <jinsungyoo@Jinsungui-MacBookPro.local>
2025-07-10 00:34:31 +1200

    Login/Signup Page Added

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,5 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
-
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("❌ supabaseUrl and supabaseAnonKey are required! Check environment variables in your deploy settings.");
+  }
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
