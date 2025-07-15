15d7933fe85256315f0051e0f9636661ac085d35
Jinsung Yoo <preketchup@gmail.com>
2025-07-16 01:14:57 +1200

    SignUp Page Update

--- public/lib/supabaseClient.js
+++ public/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://vmthbmrxnkatbmtwhtxo.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtdGhibXJ4bmthdGJtdHdodHhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NzM5MTEsImV4cCI6MjA2NzU0OTkxMX0.irH6VBASyG4iTkrRf9Jfk5sV2GTpTQe7WKrsADYR8p0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
