0d9375f9a5c9817ce41065a915d0d2cf0383298e
Jinsung Yoo <preketchup@gmail.com>
2025-07-22 16:33:21 +1200

    Deploy Astro to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless"; // ✅ 서버리스 모드
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://seorosaranghara.com", // 실제 배포 도메인 입력
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel(), // ✅ Vercel 용 어댑터로 변경
 });
